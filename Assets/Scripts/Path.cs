05a40a434f28d6a909d1f9a6c14be1f6b051dfdb
SeerNT <63809506+SeerNT@users.noreply.github.com>
2024-05-17 19:30:03 +0300

    Основной проект с изменением скрипта

--- Assets/Scripts/Path.cs
+++ Assets/Scripts/Path.cs
@@ -13,6 +13,10 @@ public class Path : MonoBehaviour
     [SerializeField] private GameObject deskDetails;
     [SerializeField] private GameObject deskMain;
     [SerializeField] private GameObject detailPrefab;
+    [SerializeField] private Animator manipulatorAnimator;
+    [SerializeField] private GameObject manipulator;
+    [SerializeField] private GameObject machine;
+    [SerializeField] private Animator mold;
     [Header("Settings")]
     [SerializeField] private GameObject[] pathPoints;
     [SerializeField] private float[] timings;
@@ -20,6 +24,9 @@ public class Path : MonoBehaviour
 
     private Vector3 actualPos;
     private int x;
+
+    private int detailsAmount = 0;
+
     [Header("UI")]
     [SerializeField] private Button startBut;
     [SerializeField] private TextMeshProUGUI stageText;
@@ -43,7 +50,10 @@ public class Path : MonoBehaviour
 
         stageText.text = stagesDict[x];
 
-        startBut.onClick.AddListener(delegate { StartCoroutine(NextPhase(timings[x])); });
+        startBut.onClick.AddListener(delegate { 
+            if(x == 0)
+                StartCoroutine(NextPhase(timings[x])); 
+        });
     }
 
     public IEnumerator NextPhase(float time)
@@ -64,6 +74,8 @@ public class Path : MonoBehaviour
             objForDelivery = null;
             x = 0;
             stageText.text = stagesDict[x];
+
+            detailsAmount++;
         }
 
         if(x == 1)
@@ -72,14 +84,64 @@ public class Path : MonoBehaviour
             objForDelivery.transform.SetParent(delivery.transform);
             objForDelivery.transform.localPosition = new Vector3(-242, 293, 921);
         } 
-        if(x == 3)
+        if(x == 2)
         {
+            // Wait for robot arriving
+            yield return new WaitForSeconds(1.5f);
+            manipulatorAnimator.Play("RotateToPickUp");
+            yield return new WaitForSeconds(0.6f);
+            objForDelivery.transform.SetParent(manipulator.transform);
+            objForDelivery.transform.localPosition = new Vector3(-1045.7f, 990.8f, 252.5f);
+            objForDelivery.transform.localEulerAngles = new Vector3(40.397f, 96.159f, 4.339f);
+            yield return new WaitForSeconds(1.1f);
             objForDelivery.transform.SetParent(deskMain.transform);
             objForDelivery.transform.localPosition = new Vector3(0.626f, 0.481f, -0.06f);
+            objForDelivery.transform.localEulerAngles = new Vector3(0, 90, 0);
+        }
+        if(x == 3)
+        {
+            objForDelivery.transform.SetParent(manipulator.transform);
+            objForDelivery.transform.localPosition = new Vector3(-1045.7f, 990.8f, 252.5f);
+            objForDelivery.transform.localEulerAngles = new Vector3(40.397f, 96.159f, 4.339f);
+            yield return new WaitForSeconds(1.2f);
+            manipulatorAnimator.Play("RotateToSend");
+            yield return new WaitForSeconds(0.6f);
+            objForDelivery.transform.SetParent(machine.transform);
+            objForDelivery.transform.localPosition = new Vector3(-1979f, 2549f, 2398f);
+            objForDelivery.transform.localEulerAngles = new Vector3(0f, 0f, 0f);
+            objForDelivery.transform.localScale = new Vector3(200f, 200f, 200f);
+            yield return new WaitForSeconds(0.8f);
+            mold.Play("Press");
+            yield return new WaitForSeconds(0.9f);
+            Destroy(objForDelivery.transform.GetChild(0).gameObject);
+            Instantiate(detailPrefab, objForDelivery.transform);
+            yield return new WaitForSeconds(0.6f);
+            manipulatorAnimator.Play("RotateToDeskFromMachine");
+            yield return new WaitForSeconds(0.6f);
+            objForDelivery.transform.SetParent(manipulator.transform);
+            objForDelivery.transform.localPosition = new Vector3(-1045.7f, 990.8f, 252.5f);
+            objForDelivery.transform.localEulerAngles = new Vector3(40.397f, 96.159f, 4.339f);
+            yield return new WaitForSeconds(1.2f);
+            objForDelivery.transform.SetParent(deskMain.transform);
+            objForDelivery.transform.localPosition = new Vector3(0.626f, 0.481f, -0.06f);
+            objForDelivery.transform.localEulerAngles = new Vector3(0, 90, 0);
         }
         if(x == 4)
         {
-            objForDelivery = detailPrefab;
+            // Wait for robot arriving
+            yield return new WaitForSeconds(1.5f);
+            objForDelivery.transform.SetParent(manipulator.transform);
+            objForDelivery.transform.localPosition = new Vector3(-1045.7f, 990.8f, 252.5f);
+            objForDelivery.transform.localEulerAngles = new Vector3(40.397f, 96.159f, 4.339f);
+            manipulatorAnimator.Play("RotateToPickUp");
+            yield return new WaitForSeconds(1.1f);
+            objForDelivery.transform.SetParent(delivery.transform);
+            objForDelivery.transform.localPosition = new Vector3(-242, 293, 921);
+        }
+        if(x == 5)
+        {
+            objForDelivery.transform.SetParent(delivery.transform);
+            objForDelivery.transform.localPosition = new Vector3(-242, 293, 921);
         }
     }
 
